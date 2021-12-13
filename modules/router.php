2c1c4c68ea910d63d97874d52ff6894cefecbc86
D23Alex <90474096+D23Alex@users.noreply.github.com>
2021-12-13 19:33:42 +0300

    router fix

--- modules/router.php
+++ modules/router.php
@@ -4,44 +4,9 @@ if ($request_path && $request_path[-1] == '/')
     $request_path = substr($request_path, 0,
         strlen($request_path) - 1);
 $result = [];
-if (preg_match('/^cats\/(\w+)$/', $request_path, $result) === 1) {
-    $ctr = new \Controllers\Spending();
-    $ctr->by_cat($result[1]);
-} else if (preg_match('/^users\/(\w+)$/', $request_path, $result) === 1) {
+if (preg_match('/^users\/(\w+)$/', $request_path, $result) === 1) {
     $ctr = new \Controllers\Spending();
     $ctr->by_user($result[1]);
-}  else if (preg_match('/^(\d+)$/', $request_path, $result) === 1) {
-    $index = (integer)$result[1];
-    $ctr = new \Controllers\Spending();
-    $ctr->item($index);
-}  else if (preg_match('/^users\/(\w+)\/pictures\/add$/',
-    $request_path, $result) === 1) {
-    $ctr = new \Controllers\Spending();
-    $ctr->add($result[1]);
-} else if (preg_match('/^users\/(\w+)\/pictures\/(\d+)\/edit$/',
-    $request_path, $result) === 1) {
-    $ctr = new \Controllers\Spending();
-    $ctr->edit($result[1], (integer)$result[2]);
-} else if (preg_match('/^users\/(\w+)\/pictures\/(\d+)\/delete$/',
-    $request_path, $result) === 1) {
-    $ctr = new \Controllers\Spending();
-    $ctr->delete($result[1], (integer)$result[2]);
-} else if (preg_match('/^(\d+)\/comments\/(\d+)\/edit$/',
-    $request_path, $result) === 1) {
-    $picture_index = (integer)$result[1];
-    $comment_index = (integer)$result[2];
-    $ctr = new \Controllers\Comments();
-    $ctr->edit($picture_index, $comment_index);
-} else if (preg_match('/^(\d+)\/comments\/(\d+)\/delete$/',
-    $request_path, $result) === 1) {
-    $picture_index = (integer)$result[1];
-    $comment_index = (integer)$result[2];
-    $ctr = new \Controllers\Comments();
-    $ctr->delete($picture_index, $comment_index);
-} else if (preg_match('/^users\/(\w+)\/account\/delete$/',
-    $request_path, $result) === 1) {
-    $ctr = new \Controllers\Account();
-    $ctr->delete($result[1]);
 } else if (preg_match('/^users\/(\w+)\/account\/edit$/',
     $request_path, $result) === 1) {
     $ctr = new \Controllers\Account();
